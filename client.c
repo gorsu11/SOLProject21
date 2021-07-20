9abce76578c1ac3b0356036c97fef2c84a3e1125
gorsu11 <gianlucaorsucci7@gmail.com>
2021-07-20 17:23:28 +0200

    implementato h, p, f
    Manca da aggiungere la lista

--- client.c
+++ client.c
@@ -15,14 +15,46 @@ int flag_stampa = 0;
 int main(int argc, char* argv[]){
     char opt;
 
+    char *farg;
+    int checkH = 0, checkP = 0, checkF = 0;
+
     while((opt = getopt(argc, argv, "hpf:w:W:r:R:d:D:t:c:l:u:")) != -1){
         switch(opt){
             case 'h':
+                if(checkH == 0){
+                    if(DEBUGCLIENT) printf("Opzione -h\n");
+                    checkH = 1;
+                    //aggiungere alla lista
+                }
+                else{
+                    printf("L'opzione -h non puo' essere ripetuta\n");
+                }
+                if(DEBUGCLIENT) fprintf(stdout, "Inserito %c\n", opt);
                 break;
+
             case 'p':
+                if (checkP == 0) {
+                    if(DEBUGCLIENT) printf("Opzione -p\n");
+                    checkP = 1;
+                    //aggiungere alla lista
+                }else{
+                    printf("L'opzione -p non puo' essere ripetuta\n");
+                }
+                if(DEBUGCLIENT) fprintf(stdout, "Inserito %c\n", opt);
                 break;
+
             case 'f':
+                if (checkF == 0) {
+                    checkF = 1;
+                    farg = optarg;
+                    //aggiungere alla lista
+                    if(DEBUGCLIENT) printf("Opzione -f con argomento : %s\n",farg);
+                }else{
+                    printf("L'opzione -f non puo' essere ripetuta\n");
+                }
+                if(DEBUGCLIENT) fprintf(stdout, "Inserito %c\n", opt);
                 break;
+
             case 'w':
                 break;
             case 'W':
