4ffe5abab3b7fcd09e491d1f309c2279acb0f839
gorsu11 <gianlucaorsucci7@gmail.com>
2021-08-13 11:35:08 +0200

    implementata readNFiles, writeFile, appendToFile
    iniziata lockFile, ma da finire e iniziare unlockFile

--- interface.c
+++ interface.c
@@ -160,18 +160,245 @@ int readFile(const char* pathname, void** buf, size_t* size){
 }
 
 int readNFiles(int N, const char* dirname){
-    return 0;
+    if(connection_socket == 0){
+        errno = EINVAL;
+        return -1;
+    }
+
+    if(dirname != NULL){       //creo la directory se non esiste
+        mkdir_p(dirname);
+    }
+
+    //INVIA IL COMANDO AL SERVER
+    char bufsend[LEN];
+    memset(bufsend, 0, LEN);
+    sprintf(bufsend, "readNFile,%d", N);
+    SYSCALL_EXIT("writen", notused, writen(sockfd, bufsend, LEN), "writen", "")
+
+    //RICEVE IL NUMERO DI FILE CHE IL SERVER INVIA
+    char bufrec[LEN];
+    memset(bufrec, 0, LEN);
+    SYSCALL_EXIT("readn", notused, readn(sockfd, bufrec, LEN), "readn", "");
+
+    char* t = strtok(response, ",");
+
+    if(strcmp(t, "-1") == 0){
+        t = strtok(NULL, ",");
+        errno = atoi(t);
+        return -1;
+    }
+
+    int number = atoi(t);
+
+    //Invio la conferma al server
+    SYSCALL_EXIT("writen", notused, writen(sockfd, "ok", LEN), "writen", "");
+
+    for(int i=0; i<number; i++){
+        //RICEVO PATH
+        char path[PATH_MAX];
+        memset(path, 0, PATH_MAX);
+        SYSCALL_EXIT("readn", notused, readn(sockfd, path, LEN), "readn", "");
+
+        char* t1 = strtok(path, ",");
+        if(strcmp(t1, "-1") == 0){
+            t1 = strtok(NULL, ",");
+            errno = atoi(t1);
+            return -1;
+        }
+
+        //Invio la conferma al server
+        SYSCALL_EXIT("writen", notused, writen(sockfd, "ok", LEN), "writen", "");
+
+        //RICEVO SIZE
+        char ssize[LEN];
+        memset(ssize, 0, LEN);
+        SYSCALL_EXIT("readn", notused, readn(sockfd, ssize, LEN), "readn", "");
+
+        char *t2 = strtok(ssize,",");
+        if (strcmp(t2,"-1")==0) { //ERRORE DAL SERVER
+            t2 = strtok(NULL,",");
+            errno = atoi(t2);
+            return -1;
+        }
+
+        int size_file = atoi(ssize);
+        //Invio la conferma al server
+        SYSCALL_EXIT("writen", notused, writen(sockfd, "ok", LEN), "writen", "");
+
+        //RICEVO FILE
+        char* fbuf;
+        CHECKNULL(fbuf, malloc(size_file*sizeof(char)), "malloc fbuf");
+        SYSCALL_EXIT("readn", notused, readn(sockfd, fbuf, size_file), "readn", "");
+
+        char *t3 = strtok(fbuf,",");
+        if (t3!=NULL) {
+            if (strcmp(t3,"-1")==0) { //ERRORE DAL SERVER
+                t3 = strtok(NULL,",");
+                errno = atoi(t3);
+                return -1;
+            }
+        }
+
+        if(dirname != NULL){
+            //salva in dir
+            char sp[PATH_MAX];
+            memset(sp, 0, PATH_MAX);
+            char* file_name = basename(path);
+            sprintf(sp,"%s/%s",dirname,file_name);
+
+            FILE* of;
+            of = fopen(sp,"w");
+            if (of==NULL) {
+                printf("Errore aprendo il file\n");
+            } else {
+                fprintf(of,"%s",fbuf);
+                fclose(of);
+            }
+        }
+        free(fbuf);
+    }
+
+    return number;
 }
 
 int writeFile(const char* pathname, const char* dirname){
+    if(!pathname || !dirname || connection_socket == 0){
+        errno = EINVAL;
+        return -1;
+    }
+    FILE *fi;
+    int size_file;
+
+    if((fi = fopen(pathname, "rb")) == NULL){
+        errno = EINVAL;
+        return -1;
+    }
+
+    char buffer[LEN];
+    memset(buffer, 0, LEN);
+    sprintf(buffer, "writeFile,%s", pathname);
+
+    SYSCALL_EXIT("writen", notused, writen(sockfd, buffer, LEN), "writen", "");
+    SYSCALL_EXIT("readn", notused, readn(sockfd, response, LEN), "readn", "");
+
+    char* t = strtok(response, ",");
+
+    if(strcmp(t, "-1") == 0){
+        t = strtok(NULL, ",");
+        errno = atoi(t);
+        return -1;
+    }
+
+    struct stat st;
+    stat(pathname, &st);
+    size_file = (int) st.st_size;
+
+    if(size_file > 0){
+        char* file_buffer;
+        CHECKNULL(file_buffer, malloc((size_file+1)*sizeof(char)), "malloc write");
+
+        size_t newLen = fread(file_buffer, sizeof(char), size_file, fi);
+        if(ferror(fi) != 0){
+            errno = EINVAL;
+            free(file_buffer);
+            return -1;
+        }
+        else{
+            file_buffer[newLen++] = '\0';
+        }
+        fclose(fi);
+
+        //INVIO SIZE FILE
+        char tmp[LEN];
+        memset(tmp, 0, LEN);
+        sprintf(tmp, "%d", size_file);
+        SYSCALL_EXIT("writen", notused, writen(sockfd, tmp, LEN), "writen", "");
+
+        //CONFERMA DAL SERVER
+        char conf[LEN];
+        memset(conf, 0, LEN);
+        SYSCALL_EXIT("readn", notused, readn(sockfd, conf, LEN), "readn", "");
+
+        //INVIO FILE
+        SYSCALL_EXIT("writen", notused, writen(sockfd, file_buffer, size_file+1), "writen", "");
+        free(file_buffer);
+
+        //RISPOSTA SERVER
+        char result[LEN];
+        memset(result, 0, LEN);
+        SYSCALL_EXIT("readn", notused, readn(sockfd, result, LEN), "readn", "");
+
+        char * t1;
+        t1 = strtok(result,",");
+
+        if (strcmp(t1,"-1")==0) { //ERRORE DAL SERVER
+            t1 = strtok(NULL,",");
+            errno = atoi(t1);
+            return -1;
+        }
+    }
+
     return 0;
 }
 
 int appendToFile(const char* pathname, void* buf, size_t size, const char* dirname){
+    if(!pathname || !dirname || connection_socket == 0){
+        errno = EINVAL;
+        return -1;
+    }
+
+    char buffer[LEN];
+    sprintf(buffer, "appendToFile,%s", pathname);
+
+    SYSCALL_EXIT("writen", notused, writen(sockfd, buffer, LEN), "writen", "");
+    SYSCALL_EXIT("readn", notused, readn(sockfd, response, LEN), "readn", "");
+
+    char* t = strtok(response, ",");
+
+    if(strcmp(t, "-1") == 0){
+        t = strtok(NULL, ",");
+        errno = atoi(t);
+        return -1;
+    }
+
+    //INVIO SIZE FILE
+    char tmp[LEN];
+    memset(tmp, 0, LEN);
+    SYSCALL_EXIT("writen", notused, writen(sockfd, tmp, LEN), "writen", "");
+
+    //CONFERMA DAL SERVER
+    char conf[LEN];
+    memset(conf, 0, LEN);
+    SYSCALL_EXIT("readn", notused, readn(sockfd, conf, LEN), "readn", "");
+
+    //INVIO I FILE
+    SYSCALL_EXIT("writen", notused, writen(sockfd, buf, size), "writen", "");
+
+    //RISPOSTA DAL SERVER
+    char result[LEN];
+    memset(result, 0, LEN);
+    SYSCALL_EXIT("readn", notused, readn(sockfd, result, LEN), "readn", "");
+
+    char * t1;
+    t1 = strtok(result,",");
+
+    if (strcmp(t1,"-1")==0) { //ERRORE DAL SERVER
+        t1 = strtok(NULL,",");
+        errno = atoi(t1);
+        //free(buf);
+        return -1;
+    }
+
     return 0;
 }
 
 int lockFile(const char* pathname){
+    if(!pathname || connection_socket == 0){
+        errno = EINVAL;
+        return -1;
+    }
+
+
     return 0;
 }
 
