d4b3446be3eeed7331f53bcb061b3fabe1c2ffb7
gorsu11 <gianlucaorsucci7@gmail.com>
2021-09-07 11:34:41 +0200

    nothing

---
+++ src/parsingFile.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../includes/util.h"
+
+#define SOCKNAME "socket_name"
+
+typedef struct config_File{
+    int num_thread;                         //numero thread
+    size_t sizeBuff;                        //spazio di memoria
+    unsigned int num_files;                 //numero massimo dei file possibili
+    char *socket_name;                      //nome della socket
+} config;
+
+long isNumberParser(const char* s) {
+   char* e = NULL;
+   long val = strtol(s, &e, 0);
+   if (e != NULL && *e == (char)0) return val;
+   return -1;
+}
+
+config* getConfig(const char* string){
+    FILE *file;
+
+    config *configuration; //Variabile che ospita la configurazione
+    CHECKNULL(configuration, malloc(sizeof(config)), "malloc configuration");
+    char str[200];
+    char arg [100];
+    char val [100];
+
+    if((file = fopen(string, "r")) == NULL){
+        perror("Error open file");
+        return NULL;
+    }
+
+    while(fgets(str, 200, file) != NULL){
+
+        if (str[0]!='\n') {
+
+            int nt;
+            nt = sscanf (str,"%[^=] = %s",arg,val);
+            if (nt!=2) {
+                printf("Errore config file : formato non corretto\n");
+                printf ("Server avviato con parametri di default\n");
+                break;
+            }
+
+            if (strcmp(arg,"Numero thread")==0) {
+
+                int n;
+                if ((n = (int) isNumberParser(val))==-1 || n<=0) {
+                    printf("Errore config file : num_thread richiede un numero>0 come valore\n");
+                    printf ("Server avviato con parametri di DEFAULT\n");
+                    break;
+                }
+                else{
+                    configuration->num_thread = n;
+                }
+
+            } else if (strcmp(arg,"File Massimi")==0) {
+
+                int n;
+                if ((n = (int) isNumberParser(val))==-1 || n<=0) {
+                    printf("Errore config file : max_files richiede un numero>0 come valore\n");
+                    printf ("Server avviato con parametri di DEFAULT\n");
+                    break;
+                }
+                else{
+                    configuration->num_files = n;
+                }
+
+            } else if (strcmp(arg,"Dimensione Massima")==0) {
+
+                int n;
+                if ((n = (int) isNumberParser(val))==-1 || n<=0) {
+                    printf("Errore config file : max_dim richiede un numero>0 come valore\n");
+                    printf ("Server avviato con parametri di DEFAULT\n");
+                    break;
+                }
+                else{
+                    configuration->sizeBuff = n;
+                }
+
+            } else if (strcmp(arg,"Nome Socket")==0) {
+                configuration->socket_name = malloc(20 * sizeof(char));
+                strcpy(configuration->socket_name,val);
+            }
+        }
+
+    }
+    fclose(file);
+
+    return configuration;
+}
+
+
+//Nel caso di errore vado a prendere dei parametri di default
+config* default_configuration(){
+    config* configuration = malloc(sizeof(config));
+    configuration->num_files = 10;
+    configuration->num_thread = 1;
+    configuration->sizeBuff = 10000;
+    CHECKNULL(configuration->socket_name, malloc(BUFSIZE), "malloc");
+    strcmp(configuration->socket_name, SOCKNAME);
+    return configuration;
+}
+
+//Stampa la struttura
+void stampa(config* configuration){
+    printf("Numero thread: %d\n", configuration->num_thread);
+    printf("Spazio memoria: %zu\n", configuration->sizeBuff);
+    printf("Numero files: %d\n", configuration->num_files);
+    printf("Nome socket: %s\n", configuration->socket_name);
+}
+
+//Libera lo spazio in memoria
+int freeConfig(config* configuration){
+    if(configuration != NULL){
+        configuration->num_files = 0;
+        configuration->sizeBuff = 0;
+        configuration->num_thread = 0;
+        if(configuration->socket_name != NULL){
+            char *string = configuration->socket_name;
+            free(string);
+        }
+        free(configuration);
+        return 0;
+    }
+    return -1;
+}
