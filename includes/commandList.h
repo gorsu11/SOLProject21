412b5e0be408ead8f96fb292b74b922049561c96
gorsu11 <gianlucaorsucci7@gmail.com>
2021-09-07 11:32:37 +0200

    debug

---
+++ includes/commandList.h
@@ -0,0 +1,11 @@
+#ifndef commandlist_h
+#define commandlist_h
+
+//------------ NON UTILIZZATA -------------//
+
+//void addList(node ** list,char * cmd,char * arg);
+//void printList(node * list);
+//int containCMD(node ** list, char * cmd, char ** arg);
+//void freeList(node ** list);
+
+#endif /* commandlist_h */
