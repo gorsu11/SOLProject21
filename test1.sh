213b37a950766e48bb540dc08562979cb1f027d4
gorsu11 <gianlucaorsucci7@gmail.com>
2021-09-11 17:20:40 +0200

    Implementati test1 e test2

--- test1.sh
+++ test1.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+echo "*********************************************"
+echo "*************** AVVIO TEST 1 ****************"
+echo "*********************************************"
 
-echo "Avvio script client"
-pid=$!
+#avvio il server
+./server configTest1/config.txt &
 sleep 2s #aspetto che si sia avviato il server
 
-#TESTA GESTIONE MEMORIA SERVER
-./client -f "socket_name" -p -t 200 -W testfile/prova4.txt,testfile/big.txt,testfile/monologhi/Joker.txt -w testfile/poesie,3 -r /Users/gorsu/Desktop/SOLProject21/SOLProject21/testfile/monologhi/Joke.txt,/Users/gorsu/Desktop/SOLProject21/SOLProject21/testfile/big.txt -R 2 -d downloadTest1 -c /Users/gorsu/Desktop/SOLProject21/SOLProject21/testfile/monologhi/Joker.txt
+#mi salvo il pid
+SERVER_PID=$!
 
-sleep 1s
+#TESTA GESTIONE MEMORIA SERVER
+./client -f "socket_name" -p -t 200 -W testfile/prova4.txt,testfile/big.txt,testfile/monologhi/Joker.txt -w testfile/poesie,3 -r /Users/gorsu/Desktop/SOLProject21/SOLProject21/testfile/monologhi/Joker.txt,/Users/gorsu/Desktop/SOLProject21/SOLProject21/testfile/big.txt -R 2 -d downloadTest1 -c /Users/gorsu/Desktop/SOLProject21/SOLProject21/testfile/monologhi/Joker.txt
+
+sleep 2s
 #sighup al server
-killall SIGHUP $pid
-wait $pid
+kill -s SIGHUP $SERVER_PID
+wait $SERVER_PID
+
+echo "*********************************************"
+echo "************** TEST 2 SUPERATO **************"
+echo "*********************************************"
+exit 0
+
