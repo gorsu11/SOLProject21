6f328fc9dc44b7f82950402872c3d8ff3fa9e9e2
gorsu11 <gianlucaorsucci7@gmail.com>
2021-09-27 11:41:06 +0200

    aggiunto fileLog al parsing del config

--- includes/parsingFile.h
+++ includes/parsingFile.h
@@ -4,12 +4,14 @@
 #include "util.h"
 
 #define SOCKNAME "socket_name"
+#define FILE_LOG "log.txt"
 
 typedef struct config_File{
     int num_thread;                         //numero thread
     size_t sizeBuff;                        //spazio di memoria
     unsigned int num_files;                 //numero massimo dei file possibili
     char *socket_name;                      //nome della socket
+    char* fileLog;
 } config;
 
 long isNumberParser(const char* s);
